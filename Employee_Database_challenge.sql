130367a9ed40bc7ca84a8fcbad4c1f45f77ac79f
hijadelsol28 <118699700+hijadelsol28@users.noreply.github.com>
2023-01-29 17:56:30 -0700

    Add files via upload

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,64 @@
+--The Number of Retiring Employees by Title.
+SELECT e.emp_no,
+	e.first_name, 
+	e.last_name, 
+	ti.title, 
+	ti.from_date, 
+	ti.to_date	
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+	WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) 
+	rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+--retrieve the number of employees by their most recent job title who are about to retire
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT(title) DESC;
+
+-----------------------
+--Deliverable 2
+-----------------------
+
+--Mentorship Eligibility
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+---------------------
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+     emp_no INT NOT NULL,
+	 dept_no VARCHAR(40) NOT NULL,
+	 from_date DATE,
+	 to_date DATE
+    -- PRIMARY KEY (emp_no)
+	);
